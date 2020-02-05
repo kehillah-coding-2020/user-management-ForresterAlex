67cb8f86cc1b5375b8182de427703273aeca7e6b
A Forrester <aforrester2022@kehillahstudent.org>
2020-02-05 12:55:24 -0800

    write basic logout fucntion in a file to expand apon if needed, may get merged into main if fucntion doesn't become more comlex

---
+++ logout.py
@@ -0,0 +1,11 @@
+#Logout function to wipe active userdata
+
+#Modue Imports
+
+#File Imports
+
+#Register Function
+
+def logout():
+    User = None
+    return User
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import hashlib
 #Function Imports
 from register import register
 from login import login
-
+from logout import logout
 #Main navigation prompt
 
 
@@ -15,13 +15,15 @@ while not exit:
     print("Current User: " + str(User))
     print('1. Register')
     print('2. Login')
-    print('3. Different Stuff')
-    print('4. Exit')
+    print('3. Logout')
+    print('6. Exit')
 
     s = input('Make a selection: ')
-    if s == '4':
+    if s == '6':
         exit = True
     elif s == '1':
         User = register()
     elif s == '2':
         User = login()
+    elif s == '3':
+        User = logout()
